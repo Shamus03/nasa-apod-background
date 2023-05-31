61b8fd0818747b7ae1681835f9cf25c853f65fac
Shamus Taylor <jtaylor@labattfood.com>
2023-05-31 09:30:25 -0500

    Ignore videos

--- src/opt/nasa-apod-background/nasa-apod-background.fish
+++ src/opt/nasa-apod-background/nasa-apod-background.fish
@@ -22,22 +22,28 @@ set apod_url (echo $apod | jp -u url)
 set apod_hdurl (echo $apod | jp -u hdurl)
 set apod_explanation (echo $apod | jp -u explanation)
 set apod_date (echo $apod | jp -u date)
+set apod_media_type (echo $apod | jp -u media_type)
 
 set last_downloaded_date (cat $date_file 2>/dev/null)
 
 if string length -- $_flag_force; or test "$apod_date" != "$last_downloaded_date"
   rm -f $date_file
-  if not wget -O $img_full $apod_hdurl
-    echo "HD image not found: $apod_hdurl"
-    echo "Will download low quality version"
-    wget -O $img_full $apod_url
+
+  if test $apod_media_type = video
+    echo "Today's APOD is a video, will not download"
+  else
+    if not wget -O $img_full $apod_hdurl
+      echo "HD image not found: $apod_hdurl"
+      echo "Will download low quality version"
+      wget -O $img_full $apod_url
+    end
+
+    # Resize full image to fit screen
+    and convert -background black -resize 1920x1080 -extent 1920x1080 -gravity center -quiet $img_full $img_desktop
+
+    # Create captioned image for lock screen
+    and convert $img_desktop \( -background none -size 1920x -fill white -undercolor black -pointsize 20 caption:(string replace --all "'" "\'" $apod_explanation) \) -gravity southwest -compose over -composite $img_lock
   end
-
-  # Resize full image to fit screen
-  and convert -background black -resize 1920x1080 -extent 1920x1080 -gravity center -quiet $img_full $img_desktop
-
-  # Create captioned image for lock screen
-  and convert $img_desktop \( -background none -size 1920x -fill white -undercolor black -pointsize 20 caption:(string replace --all "'" "\'" $apod_explanation) \) -gravity southwest -compose over -composite $img_lock
 
   and echo $apod_date > $date_file
 end
